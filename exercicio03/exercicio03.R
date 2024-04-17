7c53adcd1b88faf94fdda5d0527a2c6c57dd7cbf
Rafael Nascimento <rafael124715@gmail.com>
2024-04-17 13:44:53 -0300

    item 02 completo

--- exercicio03/exercicio03.R
+++ exercicio03/exercicio03.R
@@ -8,7 +8,7 @@ tam_amostra = 600
 
 amostra = sample_n(dados, tam_amostra)
 
-### Função para cálculo do coeficiente de variação
+## Função para cálculo do coeficiente de variação
 cof_var <- function(values){
   return(100*sd(values)/mean(values))
 }
@@ -38,9 +38,34 @@ by(amostra$int_memory, amostra$blue, cof_var)
 #2 Faça os gráficos de caixa das análises dos itens 1a, 1b, 1c e tire conclusões. #########
 ###########################################################################################
 
+## Mudança de valores de variáveis para melhor interpretação
+amostra$touch_screen = case_match(amostra$touch_screen, 0~"Sem Touch", 1~"Com Touch")
+amostra$blue = case_match(amostra$blue, 0~"Sem Bluetooth", 1~"Com Bluetooth")
+
 ## (a)  Gráfico de Caixa: “battery_power” x “touch_screen”;
+boxplot(battery_power~touch_screen,
+        data = amostra,
+        main = "Comparação de Bateria: Celular com e sem Touch",
+        xlab = "Touch Screen",
+        ylab = "Capacidade da Bateria ( mAh )",
+        col  = c("lightgreen","pink"),
+        ylim = c(200, 2200))
 
 ## (b)  Gráfico de Caixa: “m_dep” x “touch_screen”;
+boxplot(m_dep~touch_screen,
+        data = amostra,
+        main = "Comparação de Profundidade: Celular com e sem Touch",
+        xlab = "Touch Screen",
+        ylab = "Profundidade do Celular ( cm )",
+        col  = c("lightgreen","pink"),
+        ylim = c(0.0, 1.2))
+
 
 ## (c)  Gráfico de Caixa: “int_memory” x “blue”.
-
+boxplot(int_memory~blue,
+        data = amostra,
+        main = "Comparação de Memória Interna: Celular com e sem Bluetooth",
+        xlab = "Bluetooth",
+        ylab = "Memória Interna ( GB )",
+        col  = c("lightblue","lightyellow"),
+        ylim = c(0,70))
