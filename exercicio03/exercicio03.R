70147b7a84d07a3597af92b5db9ee314cd8b9a95
Rafael Nascimento <rafael124715@gmail.com>
2024-04-17 11:04:35 -0300

    adicionando comentários

---
+++ exercicio03/exercicio03.R
@@ -0,0 +1,22 @@
+#0 Leitura da base de dados
+dados <- read.csv2("mobile.cvs", dec=".")
+
+###########################################################################################
+#1 Calcule a média, desvio padrão e coeficiente de variação para as seguintes variáveis: ##
+###########################################################################################
+
+## (a)  “battery_power”, em função das categorias da variável “touch_screen”;
+
+## (b)  “m_dep”, em função das categorias da variável “touch_screen”;
+
+## (c)  “int_memory”, em função das categorias da variável “blue”.
+
+###########################################################################################
+#2 Faça os gráficos de caixa das análises dos itens 1a, 1b, 1c e tire conclusões. #########
+###########################################################################################
+
+## (a)  Gráfico de Caixa: “battery_power” x “touch_screen”;
+
+## (b)  Gráfico de Caixa: “m_dep” x “touch_screen”;
+
+## (c)  Gráfico de Caixa: “int_memory” x “blue”.
