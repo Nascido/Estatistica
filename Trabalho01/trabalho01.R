6bd81e46607d4001f85a1e9d6d09ff4fee2d7e5c
Rafael Nascimento <rafael124715@gmail.com>
2024-04-26 19:41:28 -0300

    Tratamento da database 01

--- Trabalho01/trabalho01.R
+++ Trabalho01/trabalho01.R
@@ -1,8 +1,23 @@
 library(readr)
 library(doBy)
+library(dplyr)
 
-#00 Leitura de dados
+#00 Leitura e Tratamento de dados
 stroke_data <- read_csv("healthcare-dataset-stroke-data.csv")
+
+# Exclusão de Oojeto na Base de Dados
+stroke_data = stroke_data[stroke_data$gender!="Other",]
+
+# Mudança de status das variáveis
+stroke_data$hypertension = case_match(stroke_data$hypertension, 0~"Não", 1~"Sim")
+stroke_data$heart_disease = case_match(stroke_data$heart_disease, 0~"Não", 1~"Sim")
+stroke_data$ever_married = case_match(stroke_data$ever_married, "No"~"Não", "Yes"~"Sim")
+stroke_data$Residence_type = case_match(stroke_data$Residence_type, "Urban"~"Urbano", "Rural"~"Rural")
+stroke_data$stroke = case_match(stroke_data$stroke, 0~"Não", 1~"Sim")
+stroke_data$gender = case_match(stroke_data$gender, "Female"~"Feminino", "Male"~"Masculino")
+stroke_data$smoking_status = case_match(stroke_data$smoking_status, "formerly smoked"~"Fumou",
+                                        "never smoked"~"Nunca Fumou",
+                                        "smokes"~"Fuma", "Unknown"~"S/ info")
 
 
 ## Fuções para cálculo da média, desvio padrão e coeficiente de variação
