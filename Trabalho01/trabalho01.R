45dc6b047499c73fa38cf84272f2d340b60e03b0
Rafael Nascidmento (Windows) <rafael124715@gmail.com>
2024-04-27 17:22:02 -0300

    Inicio da analise de Dados

--- Trabalho01/trabalho01.R
+++ Trabalho01/trabalho01.R
@@ -37,46 +37,6 @@ stroke_data = select(stroke_data, -bmi)
 
 
 ################################################################################
-##01 Análise dos Dados #########################################################
+##01 Análise de Dados ##########################################################
 ################################################################################
 
-
-
-## Fuções para cálculo da média, desvio padrão e coeficiente de variação
-cof_var <- function(values){
-  return(100*sd(values)/mean(values))
-}
-avaliacao <- function(x){
-  c(media=mean(x), desvio=sd(x), cof=cof_var(x))
-}
-
-#01 Analise da quantidade de observações para cada variável quantitativa
-table(stroke_data$stroke)
-
-table(stroke_data$gender)
-
-table(stroke_data$hypertension)
-
-table(stroke_data$heart_disease)
-
-table(stroke_data$ever_married)
-
-table(stroke_data$smoking_status)
-
-table(stroke_data$work_type)
-
-table(stroke_data$Residence_type)
-
-
-#Gráficos de Entendimento
-
-boxplot(age~stroke,
-        data = stroke_data)
-
-boxplot(age~smoking_status,
-        data = stroke_data)
-
-boxplot(avg_glucose_level~stroke,
-        data = stroke_data)
-
-boxplot()
